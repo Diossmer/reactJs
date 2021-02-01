71c051bd67562d3d2312c3deea58b2ceade897ec
Diossmer Villamizar <Diossmer@gmail.com>
2021-02-01 15:33:21 -0400

    aprende un poco de React

---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+export function Navigation() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a href="eslint-disable-next-line" className="navbar-brand mb-0 h1">{this.props.titulo}</a>
+          </div>
+        </nav>
+    );
+  }
