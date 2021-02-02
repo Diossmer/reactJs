b08268dd0a8c8c73293806317ad62a70d6176ef8
Diossmer Villamizar <Diossmer@gmail.com>
2021-02-01 21:17:23 -0400

    desde cero

--- src/components/Navigation.js
+++
@@ -1,9 +0,0 @@
-export function Navigation() {
-    return (
-        <nav className="navbar navbar-dark bg-dark">
-          <div className="container-fluid">
-            <a href="eslint-disable-next-line" className="navbar-brand mb-0 h1">{this.props.titulo}</a>
-          </div>
-        </nav>
-    );
-  }
